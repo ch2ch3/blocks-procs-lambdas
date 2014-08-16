91a8d228f8f0662744888ce4a8ae2253fc17a1dd
ETHEL NG <ethel.ng@gmail.com>
2014-08-16 22:33:31 +0100

    Wrote first test

--- spec/blocks_procs_lambdas_spec.rb
+++ spec/blocks_procs_lambdas_spec.rb
@@ -4,4 +4,13 @@ describe Array do
 
 	let (:array) { [1, 2, 3, 4, 5] }
 
+	context "map method" do
+
+		it "fails if not called on an array" do
+			string = String.new
+			expect{ string.map }.to raise_error
+		end
+
+	end
+
 end
