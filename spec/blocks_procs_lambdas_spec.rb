710aaf5afcc8cbf3dee435e2e9a2319c3d4d7a0c
ETHEL NG <ethel.ng@gmail.com>
2014-08-16 22:34:51 +0100

    Refactored first test and wrote three more; all passed.

--- spec/blocks_procs_lambdas_spec.rb
+++ spec/blocks_procs_lambdas_spec.rb
@@ -7,8 +7,19 @@ describe Array do
 	context "map method" do
 
 		it "fails if not called on an array" do
-			string = String.new
-			expect{ string.map }.to raise_error
+			expect{ String.new.map }.to raise_error
+		end
+
+		it "returns an array if a block is passed" do
+			expect(array.map { |x| x }.class).to eq Array
+		end
+
+		it "returns an enumerator if no block is passed" do
+			expect(array.map.class).to eq Enumerator
+		end
+
+		it "invokes the given block once for each element" do
+			expect(array.map{ |x| x + 10 }).to eq [11, 12, 13, 14, 15]
 		end
 
 	end
