28d81dcca2d8c202456b2b1caab8057c96959cd4
ETHEL NG <ethel.ng@gmail.com>
2014-08-16 23:30:34 +0100

    Reimplemented select method

--- lib/blocks_procs_lambdas.rb
+++ lib/blocks_procs_lambdas.rb
@@ -12,4 +12,16 @@ class Array
 		output
 	end
 
+	def select()
+		output = []
+		if block_given?
+			self.each do |element|
+				output << element if yield(element)
+			end
+		else
+			return to_enum :select
+		end
+		output
+	end
+
 end
